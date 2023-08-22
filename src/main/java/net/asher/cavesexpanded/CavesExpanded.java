092ba54ca655c7289d52c75e9b027d25a87e9877
asher <asher.o.gibbard@gmail.com>
2023-08-22 16:00:33 -0700

    Added polished and rough ruby, and polished ruby block

--- src/main/java/net/asher/cavesexpanded/CavesExpanded.java
+++ src/main/java/net/asher/cavesexpanded/CavesExpanded.java
@@ -1,5 +1,8 @@
 package net.asher.cavesexpanded;
 
+import net.asher.cavesexpanded.block.ModBlocks;
+import net.asher.cavesexpanded.item.ModItemGroups;
+import net.asher.cavesexpanded.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +14,9 @@ public class CavesExpanded implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
